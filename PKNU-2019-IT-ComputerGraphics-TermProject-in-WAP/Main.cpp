0b077133dc7be76ad2e7db507f5056b05bd72d10
SangWooJeong <jsu4838@naver.com>
2019-05-08 17:00:02 +0900

    문제에 따라 구조 구축
    메인에서 실행매개변수 따라 해당 번호 실행
    폴더,헤더 구분

---
+++ PKNU-2019-IT-ComputerGraphics-TermProject-in-WAP/Main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<GL/glut.h>
+#include "Mirror/Mirror.h"
+#include "ModelViewer/ModelViewer.h"
+
+
+int main(int argc,char* argv[])
+{
+    std::cout<<"HelloWorld!\n";
+    system("pause");
+}
